a17b8e0655e22a5922ee741ee2f7f5c8fb827e75
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2017-04-01 09:01:51 -0500

    Update with filtering

---
+++ public/javascripts/models/dataset.js
@@ -0,0 +1,64 @@
+namespace('Lobbyist.Models')
+
+class Dataset {
+  constructor (data) {
+    this.year = '2016'
+    this.recipient = null
+    this.data = data
+    this._on = {}
+  }
+
+  set (field, value) {
+    this[field] = value
+    this.trigger('change')
+  }
+
+  fetch () {
+    d3.json('./public/javascripts/datasets/data.json', (error, data) => {
+      this.data = data
+      this.trigger('change')
+    })
+  }
+
+  getRecipients () {
+    return [...new Set(
+      this.data[this.year].links
+        .map(contribution => contribution.target.id || contribution.target)
+        .filter(target => target.includes(' (Recipient)'))
+      )
+    ]
+  }
+
+  filter (data) {
+    const lobbyistLinks = data.links.filter(contribution => contribution.target.id === this.recipient)
+    const lobbyists = lobbyistLinks.map(contribution => contribution.source.id)
+    const employerLinks = data.links.filter(contribution => lobbyists.includes(contribution.target.id))
+    const employers = employerLinks.map(contribution => contribution.source.id)
+    const nodes = data.nodes.filter(({ id }) => {
+      return this.recipient === id || lobbyists.includes(id) || employers.includes(id)
+    })
+    const links = [...lobbyistLinks, ...employerLinks].map(link => {
+      return { source: link.source.id, target: link.target.id, value: link.value }
+    })
+
+    return { nodes, links }
+  }
+
+  toJSON () {
+    if (this.recipient) {
+      return this.filter(JSON.parse(JSON.stringify(this.data[this.year])));
+    } else {
+      return this.data[this.year]
+    }
+  }
+
+  on (event, callback) {
+    this._on[event] = callback
+  }
+
+  trigger (event) {
+    this._on[event](this)
+  }
+}
+
+Lobbyist.Models.Dataset = Dataset
